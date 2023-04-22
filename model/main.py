f27e0b6178a99bffe0f0838388611afd89d89496
Niki Vasan <nikivasan@gmail.com>
2023-04-22 10:56:35 -0400

    report-related updates

--- model/main.py
+++ model/main.py
@@ -28,43 +28,43 @@ def main():
     args = parser.parse_args()
     
     # ++++++++ OPTIONAL: Test Subsets of Data ++++++++ #
-    genre_options = ['Rap', 'RnB']
+    # genre_options = ['Rap', 'RnB']
 
-    data = pd.read_csv(args.input)
-    data = data[data['genre'].isin(genre_options)]
-    genres = data['genre']
-    data = data[['tempo', 'liveness']]
+    # data = pd.read_csv(args.input)
+    # data = data[data['genre'].isin(genre_options)]
+    # genres = data['genre']
+    # data = data[['tempo', 'liveness']]
 
     # ++++++++++++++++++++++++++++++++++++++++++++++++ #
     
-    # # process data table, get genre labels
-    # data, genres = processData(args.input)
+    # process data table, get genre labels
+    data, genres = processData(args.input)
 
     # Training + Validation Data: xTrain = 63% | xValidate = 27%
     xTrain, xValidate, yTrain, yValidate = model_selection.train_test_split(data, genres, train_size=0.9) 
     
-    # # KMeans
-    # print('\n++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-    # print('Running kMeans..')
-    # print('Finding optimal model parameters...')
-    # model = GenreClusterer('KMeans')
-    # # find optimal parameters
-    # bestK, params = model.find_parameters(xValidate)
-    # print('K-Values vs Silhouette Coefficients:')
-    # print(params)
-    # print('Optimal K: %s' %bestK[0])
-    # print('Training model with K value: %s' %bestK[0])
-    #  # cluster
-    # model.update(bestK[0])
-    # yHat = model.fit(xTrain)
+    # KMeans
+    print('\n++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    print('Running kMeans..')
+    print('Finding optimal model parameters...')
+    model = GenreClusterer('KMeans')
+    # find optimal parameters
+    bestK, params = model.find_parameters(xValidate)
+    print('K-Values vs Silhouette Coefficients:')
+    print(params)
+    print('Optimal K: %s' %bestK[0])
+    print('Training model with K value: %s' %bestK[0])
+     # cluster
+    model.update(bestK[0])
+    yHat = model.fit(xTrain)
 
-    # # visualize PCA plot 
-    # model.plot_components(xTrain)
+    # visualize PCA plot 
+    model.plot_components(xTrain)
 
-    # # print results
-    # print('==== Cluster profiles ===')
-    # model.generateClusterProfiles(yHat, yTrain)
-    # model.displayClusters()
+    # print results
+    print('==== Cluster profiles ===')
+    model.generateClusterProfiles(yHat, yTrain)
+    model.displayClusters()
 
     # # DBScan
     print('\n++++++++++++++++++++++++++++++++++++++++++++++++++++++')
